0dc28d6648deb2d0076545850ea471e3d1c0129d
Nils Engelbach <nils.engelbach@axoom.com>
2017-07-07 12:09:37 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var request = require('request');
 var alexa = require("alexa-app");
 
 var PORT = process.env.PORT || 8080;
@@ -26,61 +27,70 @@ alexaApp.express({
 // from here on you can setup any other express routes or middlewares as normal
 app.set("view engine", "ejs");
 
-app.pre = function(request, response, type) {
+app.pre = function (request, response, type) {
   if (request.applicationId != "amzn1.ask.skill.43cdc88b-cff1-4cd3-840c-6872b9557566") {
     // fail ungracefully
     return response.fail("Invalid applicationId");
   }
 };
 
-alexaApp.launch(function(request, response) {
+alexaApp.launch(function (request, response) {
   response.say("You launched the app!");
 });
 
 alexaApp.intent("GetNextEvent", {
-    "utterances": [
-      "wann ist die nächste Veranstaltung",
-      "wann findet die nächste Veranstaltung statt",
-      "wann ist das nächste Konzert",
-      "wann findet das nächste Konzert statt",
-      "wann das nächste Konzert ist",
-      "wann die nächste Veranstaltung ist",
-      "wann findet das nächste Event statt",
-      "was steht an"
-    ]
-  },
-  function(request, response) {
-    response.say("Die nächste Veranstaltung ist:");
+  "utterances": [
+    "wann ist die nächste Veranstaltung",
+    "wann findet die nächste Veranstaltung statt",
+    "wann ist das nächste Konzert",
+    "wann findet das nächste Konzert statt",
+    "wann das nächste Konzert ist",
+    "wann die nächste Veranstaltung ist",
+    "wann findet das nächste Event statt",
+    "was steht an"
+  ]
+},
+  function (request, response) {
+    request("http://mv-schwieberdingen.de/wp-json/events/v1/next", function (error, response, events) {
+      if (error) {
+        response.say("Die Abfrage ist fehlgeschlagen");
+      } else {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        events = JSON.parse(events);
+        //console.log(events[0].post_title);
+        response.say("Die nächste Veranstaltung ist '" + events[0].post_title + "'. Sie findet am " + events[0].EventStartDate + " statt.");
+      }
+    });
   }
 );
 
-alexaApp.intent("AMAZON.HelpIntent",{
-  "slots": {},
-  "utterances": []
-}, function(request, response) {
-  	var helpOutput = "Um die nächste Veranstaltung abzurufen frage einfach 'Musikverein Schwieberdinen was steht an'.";
-  	var reprompt = "wie kann ich dir jetzt weiterhelfen?";
-  	// AMAZON.HelpIntent must leave session open -> .shouldEndSession(false)
-  	response.say(helpOutput).reprompt(reprompt).shouldEndSession(false);
-  	return
-});
+// alexaApp.intent("AMAZON.HelpIntent",{
+//   "slots": {},
+//   "utterances": []
+// }, function(request, response) {
+//   	var helpOutput = "Um die nächste Veranstaltung abzurufen frage einfach 'Musikverein Schwieberdinen was steht an'.";
+//   	var reprompt = "wie kann ich dir jetzt weiterhelfen?";
+//   	// AMAZON.HelpIntent must leave session open -> .shouldEndSession(false)
+//   	response.say(helpOutput).reprompt(reprompt).shouldEndSession(false);
+//   	return
+// });
 
-alexaApp.intent("AMAZON.StopIntent",{
-  "slots": {},
-  "utterances": []
-}, function(request, response) {
-  	var stopOutput = "Wir sehen uns!";
-  	response.say(stopOutput)
-  	return
-});
+// alexaApp.intent("AMAZON.StopIntent",{
+//   "slots": {},
+//   "utterances": []
+// }, function(request, response) {
+//   	var stopOutput = "Wir sehen uns!";
+//   	response.say(stopOutput)
+//   	return
+// });
 
-alexaApp.intent("AMAZON.CancelIntent",{
-  "slots": {},
-  "utterances": []
-}, function(request, response) {
-  	var cancelOutput = "Vielleicht beim nächsten mal.";
-  	response.say(cancelOutput);
-  	return
-});
+// alexaApp.intent("AMAZON.CancelIntent",{
+//   "slots": {},
+//   "utterances": []
+// }, function(request, response) {
+//   	var cancelOutput = "Vielleicht beim nächsten mal.";
+//   	response.say(cancelOutput);
+//   	return
+// });
 
 app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
