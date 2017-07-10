7fba93755af9c1e31e28b93c6b3e46c377c2d7d7
Nils Engelbach <nils.engelbach@axoom.com>
2017-07-10 12:28:31 +0200

    Updated app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var rp = require('request-promise');
 var alexa = require("alexa-app");
+var moment = require('moment');
+
+moment.locale('de');
 
 var PORT = process.env.PORT || 8080;
 var app = express();
@@ -15,11 +18,11 @@ alexaApp.express({
   // verifies requests come from amazon alexa. Must be enabled for production.
   // You can disable this if you're running a dev environment and want to POST
   // things to test behavior. enabled by default.
-  checkCert: false,
+  checkCert: true,
 
   // sets up a GET route when set to true. This is handy for testing in
   // development, but not recommended for production. disabled by default
-  debug: true
+  debug: false
 });
 
 // now POST calls to /test in express will be handled by the app.request() function
@@ -27,15 +30,15 @@ alexaApp.express({
 // from here on you can setup any other express routes or middlewares as normal
 app.set("view engine", "ejs");
 
-// app.pre = function(request, response, type) {
-//   if (request.applicationId != "amzn1.ask.skill.43cdc88b-cff1-4cd3-840c-6872b9557566") {
-//     // fail ungracefully
-//     return response.fail("Invalid applicationId");
-//   }
-// };
+app.pre = function(request, response, type) {
+  if (request.applicationId != "amzn1.ask.skill.43cdc88b-cff1-4cd3-840c-6872b9557566") {
+    // fail ungracefully
+    return response.fail("Ungültige Application ID");
+  }
+};
 
 alexaApp.launch(function (request, response) {
-  response.say("You launched the app!");
+  response.say("Herzlich Willkommen beim Musikverein Schwieberdingen. Momentan können wir dir einen Ausblick auf unsere nächsten Veranstaltungen geben. Mit der Fragen 'was steht an?' und 'wann ist die nächste Veranstaltung?' kannst du die Abfrage starten. Weitere Funktionen werden nach und nach nachgerüstet. Viel Spaß!");
 });
 
 alexaApp.intent("GetNextEvent", {
@@ -54,7 +57,8 @@ alexaApp.intent("GetNextEvent", {
     return rp("http://mv-schwieberdingen.de/wp-json/events/v1/next")
       .then(function (events) {
         var event = JSON.parse(events)[0];
-        response.say("Die nächste Veranstaltung ist '" +  event.post_title + "'. Sie findet am " + event.EventStartDate + " statt.");
+        var time = moment(event.EventStartDate, 'YYYY-MM-DD HH:mm:ss'); // 2017-07-08 09:30:00
+        response.say("Die nächste Veranstaltung ist '" +  event.post_title + "'. Sie findet am " + time.format('LLLL') + " statt.");
       })
       .catch(function (err) {
         // API call failed...
